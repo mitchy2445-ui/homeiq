f418deca74f1428994dd7b5c5ea78cec8cb3cbe4
REGINALDSENIBO <seniboreginald@gmail.com>
2025-11-26 10:18:55 -0600

    Force add Prisma migrations

---
+++ prisma/migrations/20251110010615_add_listing_details_extras/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Listing" ADD COLUMN "buildingAmenitiesNotes" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "interiorNotes" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "naturalLight" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "noiseLevel" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "rulesNotes" TEXT;
---
+++ prisma/migrations/20251126161638_add_basics_summaries/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Listing" ADD COLUMN "idealRenterSummary" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "laundrySummary" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "parkingSummary" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "petSummary" TEXT;
