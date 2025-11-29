2de0e1226020d3ab94107c327f4e684d2841fe1a
REGINALDSENIBO <seniboreginald@gmail.com>
2025-11-28 19:40:45 -0600

    Forced migration for address fields + POI fields

---
+++ prisma/migrations/20251129013852_add_poi_fields/migration.sql
@@ -0,0 +1,81 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `aptUnit` on the `Listing` table. All the data in the column will be lost.
+  - You are about to drop the column `country` on the `Listing` table. All the data in the column will be lost.
+  - Made the column `postal` on table `Listing` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `province` on table `Listing` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `street` on table `Listing` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Listing" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "landlordId" TEXT,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "houseRules" TEXT,
+    "street" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "province" TEXT NOT NULL,
+    "postal" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "beds" INTEGER NOT NULL,
+    "baths" INTEGER NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'DRAFT',
+    "images" JSONB,
+    "videos" JSONB,
+    "videoUrl" TEXT,
+    "propertyType" TEXT,
+    "neighborhoodVibe" TEXT,
+    "areaType" TEXT,
+    "distanceBusMeters" INTEGER,
+    "distanceGroceryMeters" INTEGER,
+    "distanceSchoolMeters" INTEGER,
+    "distanceParkMeters" INTEGER,
+    "distancePharmacyMeters" INTEGER,
+    "distanceGymMeters" INTEGER,
+    "distanceRestaurantsMeters" INTEGER,
+    "distanceShoppingMeters" INTEGER,
+    "distanceUniversityMeters" INTEGER,
+    "distanceNightlifeMeters" INTEGER,
+    "parkingType" TEXT,
+    "petPolicy" TEXT,
+    "accessibility" JSONB,
+    "laundry" TEXT,
+    "heating" TEXT,
+    "cooling" TEXT,
+    "furnished" BOOLEAN,
+    "utilitiesIncluded" JSONB,
+    "minLeaseMonths" INTEGER,
+    "maxOccupants" INTEGER,
+    "smokingAllowed" BOOLEAN,
+    "depositCents" INTEGER,
+    "availableFrom" DATETIME,
+    "preferredTenantType" TEXT,
+    "idealRenterSummary" TEXT,
+    "petSummary" TEXT,
+    "parkingSummary" TEXT,
+    "laundrySummary" TEXT,
+    "noiseLevel" TEXT,
+    "naturalLight" TEXT,
+    "interiorNotes" TEXT,
+    "buildingAmenitiesNotes" TEXT,
+    "rulesNotes" TEXT,
+    "locationVerified" BOOLEAN NOT NULL DEFAULT false,
+    "insights" JSONB,
+    "neighborhoodNotes" TEXT,
+    "transit" TEXT,
+    "amenities" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Listing_landlordId_fkey" FOREIGN KEY ("landlordId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Listing" ("accessibility", "amenities", "areaType", "availableFrom", "baths", "beds", "buildingAmenitiesNotes", "city", "cooling", "createdAt", "depositCents", "description", "distanceBusMeters", "distanceGroceryMeters", "distanceGymMeters", "distanceParkMeters", "distancePharmacyMeters", "distanceSchoolMeters", "furnished", "heating", "houseRules", "id", "idealRenterSummary", "images", "insights", "interiorNotes", "landlordId", "laundry", "laundrySummary", "locationVerified", "maxOccupants", "minLeaseMonths", "naturalLight", "neighborhoodNotes", "neighborhoodVibe", "noiseLevel", "parkingSummary", "parkingType", "petPolicy", "petSummary", "postal", "preferredTenantType", "price", "propertyType", "province", "rulesNotes", "smokingAllowed", "status", "street", "title", "transit", "updatedAt", "utilitiesIncluded", "videoUrl", "videos") SELECT "accessibility", "amenities", "areaType", "availableFrom", "baths", "beds", "buildingAmenitiesNotes", "city", "cooling", "createdAt", "depositCents", "description", "distanceBusMeters", "distanceGroceryMeters", "distanceGymMeters", "distanceParkMeters", "distancePharmacyMeters", "distanceSchoolMeters", "furnished", "heating", "houseRules", "id", "idealRenterSummary", "images", "insights", "interiorNotes", "landlordId", "laundry", "laundrySummary", "locationVerified", "maxOccupants", "minLeaseMonths", "naturalLight", "neighborhoodNotes", "neighborhoodVibe", "noiseLevel", "parkingSummary", "parkingType", "petPolicy", "petSummary", "postal", "preferredTenantType", "price", "propertyType", "province", "rulesNotes", "smokingAllowed", "status", "street", "title", "transit", "updatedAt", "utilitiesIncluded", "videoUrl", "videos" FROM "Listing";
+DROP TABLE "Listing";
+ALTER TABLE "new_Listing" RENAME TO "Listing";
+CREATE INDEX "Listing_landlordId_idx" ON "Listing"("landlordId");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
