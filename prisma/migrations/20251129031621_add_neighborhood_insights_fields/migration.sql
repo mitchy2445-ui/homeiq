d07b54df4dc87400c90f0e41ee4e959f9d811271
REGINALDSENIBO <seniboreginald@gmail.com>
2025-12-22 13:18:31 -0600

    Reset Prisma migrations and regenerate clean init schema

---
+++ prisma/migrations/20251129031621_add_neighborhood_insights_fields/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Listing" ADD COLUMN "neighborhoodCommunity" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "neighborhoodHighlights" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "neighborhoodNoise" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "neighborhoodSafety" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "neighborhoodTransitNotes" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "neighborhoodWalkability" TEXT;
---
+++ prisma/migrations/20251222191647_full_updated_schema/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Listing" ADD COLUMN "aptUnit" TEXT;
+ALTER TABLE "Listing" ADD COLUMN "country" TEXT;
